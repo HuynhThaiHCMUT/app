aff974faf78326c79d6b370eda4f9b657dd0edf2
HuynhThaiHCMUT <thai.dinhhuynh@hcmut.edu.vn>
2023-06-26 13:15:33 +0700

    Add header bar to layout

---
+++ app/components/headerBar.tsx
@@ -0,0 +1,13 @@
+import styles from './components.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faClock, faHome, faShop} from '@fortawesome/free-solid-svg-icons'
+import HeaderTab from './headerTab'
+
+export default function HeaderBar() {
+    return <div className={styles.headerBar}>
+        <HeaderTab href='/' icon={faHome} text=""/>
+        <HeaderTab href='/store' icon={faShop} text="Kho hàng"/>
+        <HeaderTab href='/sale' icon={faCartShopping} text="Hoá đơn"/>
+        <HeaderTab href='/history' icon={faClock} text="Lịch sử"/>
+    </div>
+}   
