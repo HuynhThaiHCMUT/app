8f10eb7acb9b1929f1ca6103c2981f938dc6dd56
BadElixir <leanhkhoacd5@gmail.com>
2023-12-27 15:54:51 +0700

    khoa push code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DisplayItem from './components/displayItem';
-import { faFilter, faX } from '@fortawesome/free-solid-svg-icons';
+import { faFilter, faGreaterThan, faLessThan, faX } from '@fortawesome/free-solid-svg-icons';
 import Image from "next/image";
 import Logo from "./img/logo.jpg";
 import slider from "./img/slider.png";
@@ -20,6 +20,26 @@ export default function Home() {
     const [category, setCategory] = useState<Category[]>([]);
     const [updated, update] = useState(false);
     const [showFilter, setShowFilter] = useState(false);
+
+    const productListRef = useRef<HTMLDivElement>(null);
+
+    const scrollLeft = () => {
+        if (productListRef.current) {
+          productListRef.current.scrollBy({
+            left: -300,
+            behavior: "smooth",
+        }); // Điều chỉnh giá trị này để thay đổi khoảng cách cuộn
+        }
+      };
+    
+      const scrollRight = () => {
+        if (productListRef.current) {
+            productListRef.current.scrollBy({
+              left: 300,
+              behavior: "smooth",
+          }); // Điều chỉnh giá trị này để thay đổi khoảng cách cuộn
+          }
+      };
 
     useEffect(() => {
         async function getData() {
@@ -45,51 +65,6 @@ export default function Home() {
         getData();
     }, [updated]);
 
-//     return  <div className={styles.home}>
-//         <div className={showFilter ? styles.filterBackground : styles.hidden} onMouseDown={() => setShowFilter(false)}>
-//             <div className={styles.filterPanel} onMouseDown={(e) => e.stopPropagation()}>
-//                 <div className={styles.filterHeader}>
-//                     <h2>Bộ lọc tìm kiếm</h2>
-//                     <button onMouseDown={() => setShowFilter(false)}><FontAwesomeIcon icon={faX}/></button>
-//                 </div>
-//                 <div className={styles.filterSection}>
-//                     <h3>Phân loại sản phẩm</h3>
-//                     <div>
-//                         <button
-//                             className={(tag == "Tất cả") ? styles.activeBox : styles.filterBox}
-//                             onClick={() => setTag("Tất cả")}>{"Tất cả"}</button>
-//                         {category.map((value, index) => 
-//                         <button key={index}
-//                             className={(tag == value.name) ? styles.activeBox : styles.filterBox}
-//                             onClick={() => setTag(value.name)}>{value.name}</button>)}
-//                     </div>
-//                 </div>
-//                 {/* <div className={styles.filterSection}>
-//                     <h3>Sắp xếp sản phẩm</h3>
-//                     <div>
-//                     {sorts.map((value, index) => 
-//                         <button key={index}
-//                             className={(sort == value) ? styles.activeBox : styles.filterBox}
-//                             onClick={() => setSort(value)}>{value}</button>)}
-//                     </div>
-//                 </div> */}
-//             </div>
-//         </div>
-//         <div className={styles.container}>
-//             <input className={styles.searchBar} 
-//             placeholder='Tìm kiếm' 
-//             type='search'
-//             value={q}
-//             onChange={(e) => {
-//                 setQ(e.target.value)
-//             }}/>
-//             <button onClick={() => setShowFilter(true)}><FontAwesomeIcon icon={faFilter}/></button>
-//         </div>
-//         <div className={styles.displayList}>
-//         {data.map((value: ProductData) => <DisplayItem item={value} key={value.id}/>)}
-//         </div>
-//     </div>
-// }
  return (
     <div className={styles.home}>
       <div
@@ -205,6 +180,14 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className={styles.dealOfTheDay}>
+            <div className={styles.dealHeader}>
+                <p className={styles.dealTitle}>Danh sách sản phẩm</p>
+                <button className={styles.buttonLeft} onClick={scrollLeft}> <FontAwesomeIcon icon={faLessThan} /></button>
+                <button className={styles.buttonRight} onClick={scrollRight}><FontAwesomeIcon icon={faGreaterThan} /></button>
+            </div>
+            <div className={styles.displayList} ref={productListRef}> {data.map((value: ProductData) => <DisplayItem item={value} key={value.id} />)} </div>
+        </div>
       <div className={styles.trendSection}>
         <div className={styles.trend1}>
           <Image src={snack} alt="" className={styles.imgSnack} />
