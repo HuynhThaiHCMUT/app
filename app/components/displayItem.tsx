b5b0638038ae7945bc4798778fc8e7f8919945a0
HuynhThaiHCMUT <thai.dinhhuynh@hcmut.edu.vn>
2024-01-01 22:50:56 +0700

    add product image to front page

--- app/components/displayItem.tsx
+++ app/components/displayItem.tsx
@@ -1,11 +1,12 @@
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 import styles from './components.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { CldImage } from 'next-cloudinary'
 
-export default function DisplayItem({item} : {item: ProductData}) {
+export default function DisplayItem({product, unit} : {product: ProductData, unit: Unit}) {
     return <div className={styles.displayItem}>
-        <FontAwesomeIcon className={styles.itemIcon} icon={faImage} size="2xl"/>
-        <p>{item.name}</p>
-        {/* item.units.map((value, index) => <p key={index}>{value.name + ": " + value.price}</p>) */}
+        {(unit.image) ? <CldImage width={150} height={120} src={unit.image} alt={`Image: ${unit.image}`}/> : <FontAwesomeIcon className={styles.itemIcon} icon={faImage} size="2xl"/>}
+        <p>{product.name}</p>
+        <p>{unit.name}</p>
     </div>
 }
