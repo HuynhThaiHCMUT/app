d27f2add10214ecad3f47a66fe2aec3de23fd79a
thien.dg <thien.dong0603@hcmut.edu.vn>
2024-01-01 15:44:41 +0700

    updateFooter

--- app/components/footerBar.tsx
+++ app/components/footerBar.tsx
@@ -1,17 +1,15 @@
 import styles from './components.module.css'
-import { faBell, faCartShopping, faHome, faShop, faUser, faUserCircle} from '@fortawesome/free-solid-svg-icons'
 
-
-export default async function FooterBar() {
+export default function FooterBar() {
     return <div className={styles.footerBar}>
         <div className={styles.footerSection}>
             <div className={styles.footerColumn}>
-                <h2>Nhận ngay ưu đãi 30%!</h2>
-                <p>Đăng ký để nhận thông tin khuyến mãi</p>
+                <h2>Nhận ngàn ưu đãi!</h2>
+                <p>Đăng ký ngay để nhận thông tin khuyến mãi</p>
             </div>
             <div className={styles.footerColumn}>
                 <form className={styles.footerForm}>
-                    <input type="email" placeholder="Email của bạn"/>
+                    <input name='mail' type="email" placeholder="Địa chỉ Email của bạn"/>
                     <input type="submit" value="Đăng ký"/>
                 </form>
             </div>
@@ -25,24 +23,18 @@ export default async function FooterBar() {
             </div>
             <div className={styles.footerColumn}>
                 <h3>Liên hệ:</h3>
+                <h4>Email:</h4>
                 <p>dreamteam@hcmut.edu.vn</p>
-                <p>0123456789</p>
+                <h4>Hotline:</h4>
+                <p>SĐT: 0123456789</p>
             </div>
             <div className={styles.footerColumn}>
                 <h3>Địa chỉ:</h3>
-                <p>411, Tòa nhà H1, Đại học Bách Khoa TP.HCM</p>
-            </div>
-            <div className={styles.footerColumn}>
-                <h3>Social:</h3>
-                <ul className={styles.socialList}>
-                    <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f">Facebook</i></a></li>
-                    <li><a href="https://www.instagram.com/"><i className="fab fa-instagram">Instagram</i></a></li>
-                    <li><a href="https://www.twitter.com/"><i className="fab fa-twitter">Twitter</i></a></li>
-                </ul>
+                <p>Tòa nhà H6, Cơ sở 2, Đại học Bách Khoa TP.HCM</p>
             </div>
         </div>
         <div className={styles.footerSection}>
-            <small>© 2021 Dream Team. All rights reserved.</small>
+            <small>© 2024 Dream Team. All rights reserved.</small>
         </div>
     </div>;
 }   
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import styles from './page.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DisplayItem from './components/displayItem';
 import { faFilter, faGreaterThan, faLessThan, faX } from '@fortawesome/free-solid-svg-icons';
+import FooterBar from './components/footerBar';
 import Image from "next/image";
 import Logo from "./img/logo.jpg";
 import slider from "./img/slider.png";
@@ -88,155 +89,158 @@ export default function Home() {
     }, [updated]);
 
  return (
-    <div className={styles.home}>
-      <div
-        className={showFilter ? styles.filterBackground : styles.hidden}
-        onMouseDown={() => setShowFilter(false)}
-      >
+    <div>
+      <div className={styles.home}>
         <div
-          className={styles.filterPanel}
-          onMouseDown={(e) => e.stopPropagation()}
+          className={showFilter ? styles.filterBackground : styles.hidden}
+          onMouseDown={() => setShowFilter(false)}
         >
-          <div className={styles.filterHeader}>
-            <h2>Bộ lọc tìm kiếm</h2>
-            <button onMouseDown={() => setShowFilter(false)}>
-              <FontAwesomeIcon icon={faX} />
-            </button>
+          <div
+            className={styles.filterPanel}
+            onMouseDown={(e) => e.stopPropagation()}
+          >
+            <div className={styles.filterHeader}>
+              <h2>Bộ lọc tìm kiếm</h2>
+              <button onMouseDown={() => setShowFilter(false)}>
+                <FontAwesomeIcon icon={faX} />
+              </button>
+            </div>
+            <div className={styles.filterSection}>
+              <h3>Phân loại sản phẩm</h3>
+              <div>
+              <button
+                  className={
+                    tag === "Tất cả" ? styles.activeBox : styles.filterBox
+                  }
+                  onClick={() => {
+                    setTag("Tất cả");
+                    scrollToDealOfTheDay();
+                  }}
+                >
+                  {"Tất cả"}
+                </button>
+                {category.map((value, index) => (
+                  <button
+                  key={index}
+                  className={
+                    tag === value.name ? styles.activeBox : styles.filterBox
+                  }
+                  onClick={() => {
+                    setTag(value.name);
+                    scrollToDealOfTheDay();
+                  }}
+                >
+                  {value.name}
+                </button>
+                ))}
+              </div>
+            </div>
           </div>
+        </div>
+        <div className={styles.container}>
+          <Image src={Logo} alt="" className={styles.imageLogo} />
+          <div className={styles.nameLogo}>
+            <div className={styles.mainName}>Grocery</div>
+            <div className={styles.subName}>Dream</div>
+          </div>
+
+          <input
+            className={styles.searchBar}
+            placeholder="Tìm kiếm"
+            type="search"
+            value={q}
+            onChange={(e) => {
+              setQ(e.target.value);
+            }}
+            onKeyDown={(f) => {
+              if (f.key === 'Enter') {
+                scrollToDealOfTheDay();
+              }
+            }}
+          />
+          {/* <button onClick={() => setShowFilter(true)}>
+            <FontAwesomeIcon icon={faFilter} />
+          </button> */}
+        </div>
+      
+        <div className={styles.slider}>
           <div className={styles.filterSection}>
             <h3>Phân loại sản phẩm</h3>
             <div>
             <button
-                className={
-                  tag === "Tất cả" ? styles.activeBox : styles.filterBox
-                }
-                onClick={() => {
-                  setTag("Tất cả");
-                  scrollToDealOfTheDay();
-                }}
+                className={tag == "Tất cả" ? styles.activeBox : styles.filterBox}
+                onClick={() => handleButtonClick("Tất cả")}
               >
                 {"Tất cả"}
               </button>
               {category.map((value, index) => (
-                 <button
-                 key={index}
-                 className={
-                   tag === value.name ? styles.activeBox : styles.filterBox
-                 }
-                 onClick={() => {
-                   setTag(value.name);
-                   scrollToDealOfTheDay();
-                 }}
-               >
-                 {value.name}
-               </button>
+                <button
+                key={index}
+                className={
+                  tag == value.name ? styles.activeBox : styles.filterBox
+                }
+                onClick={() => handleButtonClick(value.name)}
+              >
+                {value.name}
+              </button>
               ))}
             </div>
           </div>
-        </div>
-      </div>
-      <div className={styles.container}>
-        <Image src={Logo} alt="" className={styles.imageLogo} />
-        <div className={styles.nameLogo}>
-          <div className={styles.mainName}>Grocery</div>
-          <div className={styles.subName}>Dream</div>
-        </div>
-
-        <input
-          className={styles.searchBar}
-          placeholder="Tìm kiếm"
-          type="search"
-          value={q}
-          onChange={(e) => {
-            setQ(e.target.value);
-          }}
-          onKeyDown={(f) => {
-            if (f.key === 'Enter') {
-              scrollToDealOfTheDay();
-            }
-          }}
-        />
-        {/* <button onClick={() => setShowFilter(true)}>
-          <FontAwesomeIcon icon={faFilter} />
-        </button> */}
-      </div>
-     
-      <div className={styles.slider}>
-        <div className={styles.filterSection}>
-          <h3>Phân loại sản phẩm</h3>
-          <div>
-          <button
-              className={tag == "Tất cả" ? styles.activeBox : styles.filterBox}
-              onClick={() => handleButtonClick("Tất cả")}
-            >
-              {"Tất cả"}
-            </button>
-            {category.map((value, index) => (
-               <button
-               key={index}
-               className={
-                 tag == value.name ? styles.activeBox : styles.filterBox
-               }
-               onClick={() => handleButtonClick(value.name)}
-             >
-               {value.name}
-             </button>
-            ))}
-          </div>
-        </div>
-        <div className={styles.sliderSection}>
-          <div className={styles.textColumn}>
-          <h3>Chào mừng đến với</h3>
-            <h2>DREAM GROCERY</h2>
-          </div>
-          <div className={styles.imageColumn}>
-            <Image src={slider} alt="" className={styles.imageSlider} />
-          </div>
-        </div>
-      </div>
-      <div ref={dealOfTheDayRef} className={styles.dealOfTheDay}>
-            <div className={styles.dealHeader}>
-                <p className={styles.dealTitle}>Danh sách sản phẩm</p>
-                <button className={styles.buttonLeft} onClick={scrollLeft}> <FontAwesomeIcon icon={faLessThan} /></button>
-                <button className={styles.buttonRight} onClick={scrollRight}><FontAwesomeIcon icon={faGreaterThan} /></button>
+          <div className={styles.sliderSection}>
+            <div className={styles.textColumn}>
+            <h3>Chào mừng đến với</h3>
+              <h2>DREAM GROCERY</h2>
             </div>
-            <div className={styles.displayList} ref={productListRef}> {data.map((value: ProductData) => <DisplayItem item={value} key={value.id} />)} </div>
-        </div>
-        <div className={styles.trendTitle}>
-        <h2>  Tiêu chí cửa hàng</h2>
-      </div>
-      <div className={styles.trendSection}>
-        <div className={styles.trend1}>
-          <Image src={snack} alt="" className={styles.imgSnack} />
-          <h3>Chất lượng sản phẩm</h3>
-          <h5>Cam kết chỉ bán sản phẩm chất lượng và đáng tin cậy,</h5>
-          <h5>tuân thủ đầy đủ các tiêu chuẩn an toàn thực phẩm.</h5>
-        </div>
-        <div className={styles.trend2}>
-          <div className={styles.trend21}>
-            <div className={styles.textTrend21}>
-              <h3>Chất lượng dịch vụ</h3>
-              <h5>Chăm sóc khách hàng tận tâm</h5>
-              <h5>Không gian cửa hàng sạch sẽ, gọn gàng. </h5>
-            </div>
-            <Image src={milo} alt="" className={styles.imgMilo} />
-          </div>
-          <div className={styles.trend22}>
-            <Image src={skippy} alt="" className={styles.imgSkippy} />
-            <div className={styles.textTrend22}>
-              <h3>Tiêu dùng "xanh" </h3>
-              <h5> Sử dụng túi tái sử dụng và cung cấp</h5>
-              <h5> các sản phẩm thân thiện với môi trường.</h5>
+            <div className={styles.imageColumn}>
+              <Image src={slider} alt="" className={styles.imageSlider} />
             </div>
           </div>
         </div>
-        <div className={styles.trend3}>
-          <Image src={water} alt="" className={styles.imgWater} />
-          <h2>Đa dạng sản phẩm </h2>
-          <h5>Cung cấp đầy đủ sản phẩm từ</h5>
-          <h5> thực phẩm, đồ khô đến hàng tiêu dùng và đồ gia dụng.</h5>
+        <div ref={dealOfTheDayRef} className={styles.dealOfTheDay}>
+              <div className={styles.dealHeader}>
+                  <p className={styles.dealTitle}>Danh sách sản phẩm</p>
+                  <button className={styles.buttonLeft} onClick={scrollLeft}> <FontAwesomeIcon icon={faLessThan} /></button>
+                  <button className={styles.buttonRight} onClick={scrollRight}><FontAwesomeIcon icon={faGreaterThan} /></button>
+              </div>
+              <div className={styles.displayList} ref={productListRef}> {data.map((value: ProductData) => <DisplayItem item={value} key={value.id} />)} </div>
+          </div>
+          <div className={styles.trendTitle}>
+          <h2>  Tiêu chí cửa hàng</h2>
+        </div>
+        <div className={styles.trendSection}>
+          <div className={styles.trend1}>
+            <Image src={snack} alt="" className={styles.imgSnack} />
+            <h3>Chất lượng sản phẩm</h3>
+            <h5>Cam kết chỉ bán sản phẩm chất lượng và đáng tin cậy,</h5>
+            <h5>tuân thủ đầy đủ các tiêu chuẩn an toàn thực phẩm.</h5>
+          </div>
+          <div className={styles.trend2}>
+            <div className={styles.trend21}>
+              <div className={styles.textTrend21}>
+                <h3>Chất lượng dịch vụ</h3>
+                <h5>Chăm sóc khách hàng tận tâm</h5>
+                <h5>Không gian cửa hàng sạch sẽ, gọn gàng. </h5>
+              </div>
+              <Image src={milo} alt="" className={styles.imgMilo} />
+            </div>
+            <div className={styles.trend22}>
+              <Image src={skippy} alt="" className={styles.imgSkippy} />
+              <div className={styles.textTrend22}>
+                <h3>Tiêu dùng "xanh" </h3>
+                <h5> Sử dụng túi tái sử dụng và cung cấp</h5>
+                <h5> các sản phẩm thân thiện với môi trường.</h5>
+              </div>
+            </div>
+          </div>
+          <div className={styles.trend3}>
+            <Image src={water} alt="" className={styles.imgWater} />
+            <h2>Đa dạng sản phẩm </h2>
+            <h5>Cung cấp đầy đủ sản phẩm từ</h5>
+            <h5> thực phẩm, đồ khô đến hàng tiêu dùng và đồ gia dụng.</h5>
+          </div>
         </div>
       </div>
+      <FooterBar/>
     </div>
   );
 }
